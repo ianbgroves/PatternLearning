62ff67658356b3961bacc0c3068c023ea90eadb7
Ian Groves <ianbgroves@gmail.com>
2024-04-05 18:01:20 +0100

    Add code to log runs and tidy up classes

--- PINNs/04_2024_pytorch_PINNs/cooling_eqn.py
+++ PINNs/04_2024_pytorch_PINNs/cooling_eqn.py
@@ -1,5 +1,5 @@
 import wandb
-
+import os
 import torch
 import sklearn
 import numpy as np
@@ -13,26 +13,28 @@ from data import generate_and_plot_training_data
 #                       name='cooling_eqn')
 
 check_memory_usage('Script start')
-
+os.makedirs('models/cooling_eqn/', exist_ok=True)
 t, T, Tenv, t0, R, timecourse, eq, temps = generate_and_plot_training_data()
 
 # Train an unregularised normal feedforward neural network with inputs t, T
 
-net = Net(1,1, loss2=None, epochs=20000, lr=1e-5).to(DEVICE)
+net = Net(1,1, loss2=None, epochs=30000, lr=1e-5).to(DEVICE)
 print('Training with no regularisation:')
-losses = net.fit(t, T)
+losses = net.fit(t, T, project='cooling_pinns', entity='shefpinns', name='cooling_eqn_vanilla')
+net.save('models/cooling_eqn/vanilla_net.pth')
 
 plt.plot(losses)
 plt.yscale('log')
-plt.show()
+# plt.show()
 
 # Add l2 regularisation, which reduces overfitting
-netreg = Net(1,1, loss2=l2_reg, epochs=20000, lr=1e-4, loss2_weight=1).to(DEVICE)
+netreg = Net(1,1, loss2=l2_reg, epochs=30000, lr=1e-4, loss2_weight=1).to(DEVICE)
 print('Training with l2 regularisation:')
-losses = netreg.fit(t, T)
-
+losses = netreg.fit(t, T, project='cooling_pinns', entity='shefpinns', name='cooling_eqn_vanilla_l2reg')
+netreg.save('models/cooling_eqn/reg_net.pth')
 plt.plot(losses)
 plt.yscale('log')
+# plt.show()
 
 # predict with both networks for comparison
 preds = net.predict(timecourse)
@@ -47,4 +49,45 @@ plt.plot(timecourse, predsreg, alpha=0.8)
 plt.legend(labels=['Equation','Training data', 'Network', 'L2 Network'])
 plt.ylabel('Temperature (C)')
 plt.xlabel('Time (s)')
-plt.show()
+# plt.show()
+
+pinn = Net(1,1, loss2=physics_loss, epochs=100000, loss2_weight=1, lr=1e-5, args=(R, Tenv, temps)).to(DEVICE)
+
+print('Training with PINN regularisation')
+losses = pinn.fit(t, T, project='cooling_pinns', entity='shefpinns', name='cooling_eqn_pinn')
+pinn.save('models/cooling_eqn/pinn.pth')
+plt.figure()
+plt.plot(losses)
+plt.yscale('log')
+# plt.show()
+
+preds = pinn.predict(timecourse)
+
+plt.figure()
+plt.plot(timecourse, temps, alpha=0.8)
+plt.plot(t, T, 'o')
+plt.plot(timecourse, preds, alpha=0.8)
+plt.legend(labels=['Equation','Training data', 'PINN'])
+plt.ylabel('Temperature (C)')
+plt.xlabel('Time (s)')
+# plt.show()
+
+netdisc = NetDiscovery(1, 1, loss2=physics_loss_discovery, loss2_weight=1, epochs=100000, lr= 5e-6, args=(Tenv, temps)).to(DEVICE)
+print('Finding parameter values')
+losses = netdisc.fit(t, T, project='cooling_pinns', entity='shefpinns', name='cooling_eqn_discovery')
+netdisc.save('models/cooling_eqn/netdisc.pth')
+plt.figure()
+plt.plot(losses)
+plt.yscale('log')
+# plt.show()
+
+preds = netdisc.predict(timecourse)
+print(netdisc.r)
+
+plt.plot(timecourse, temps, alpha=0.8)
+plt.plot(t, T, 'o')
+plt.plot(timecourse, preds, alpha=0.8)
+plt.legend(labels=['Equation','Training data', 'discovery PINN'])
+plt.ylabel('Temperature (C)')
+plt.xlabel('Time (s)')
+# plt.show()
--- PINNs/04_2024_pytorch_PINNs/pinn.py
+++ PINNs/04_2024_pytorch_PINNs/pinn.py
@@ -1,11 +1,14 @@
 import functools
 import matplotlib.pyplot as plt
 import numpy as np
+import wandb
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as thdat
 import seaborn as sns
+from utils import wandb_setup
+from tqdm import tqdm
 from utils import check_memory_usage
 sns.set_theme()
 torch.manual_seed(42)
@@ -28,6 +31,7 @@ class Net(nn.Module):
         lr=1e-3,
         loss2=None,
         loss2_weight=0.1,
+        args=None,
     ) -> None:
         super().__init__()
 
@@ -35,6 +39,7 @@ class Net(nn.Module):
         self.loss = loss
         self.loss2 = loss2
         self.loss2_weight = loss2_weight
+        self.loss2_args = args
         self.lr = lr
         self.n_units = n_units
 
@@ -55,26 +60,37 @@ class Net(nn.Module):
         out = self.out(h)
 
         return out
+    def save(self, path):
+        torch.save(self.state_dict(), path)
+    def fit(self, X, y, project, entity, name):
 
-    def fit(self, X, y):
+        # Set up wandb logging, which is nice to compare runs
+        metrics = wandb_setup(project=project, entity=entity,
+                              name=name)
+
         Xt = np_to_th(X)
         yt = np_to_th(y)
 
         optimiser = optim.Adam(self.parameters(), lr=self.lr)
         self.train()
         losses = []
-        for ep in range(self.epochs):
+        for ep in tqdm(range(0, self.epochs)):
             optimiser.zero_grad()
             outputs = self.forward(Xt)
             loss = self.loss(yt, outputs)
             if self.loss2:
-                loss += self.loss2_weight + self.loss2_weight * self.loss2(self)
+                loss += self.loss2_weight + self.loss2_weight * self.loss2(self, self.loss2_args)
             loss.backward()
             optimiser.step()
             losses.append(loss.item())
-            if ep % int(self.epochs / 10) == 0:
-                # print(f"Epoch {ep}/{self.epochs}, loss: {losses[-1]:.2f}")
-                check_memory_usage(f"Epoch {ep}/{self.epochs}, loss: {losses[-1]:.2f}")
+
+            # Log with wandb
+            wandb.log({"epoch": ep,
+                       "train_loss": loss})
+        wandb.finish()
+            # if ep % int(self.epochs / 10) == 0:
+            #     # print(f"Epoch {ep}/{self.epochs}, loss: {losses[-1]:.2f}")
+            #     check_memory_usage(f"Epoch {ep}/{self.epochs}, loss: {losses[-1]:.2f}")
         return losses
 
     def predict(self, X):
@@ -94,14 +110,31 @@ class NetDiscovery(Net):
         lr=0.001,
         loss2=None,
         loss2_weight=0.1,
+        args=None,
     ) -> None:
         super().__init__(
-            input_dim, output_dim, n_units, epochs, loss, lr, loss2, loss2_weight
+            input_dim, output_dim, n_units, epochs, loss, lr, loss2, loss2_weight, args
         )
 
         self.r = nn.Parameter(data=torch.tensor([0.]))
 
+def physics_loss(model: torch.nn.Module, args):
+    R, Tenv, Temps = args
+    ts = torch.linspace(0, 1000, steps=1000,).view(-1,1).requires_grad_(True).to(DEVICE)
+    temps = model(ts)
+    dT = grad(temps, ts)[0]
+    pde = R*(Tenv - temps) - dT
 
+    return torch.mean(pde**2)
+
+def physics_loss_discovery(model: torch.nn.Module, args):
+    Tenv, Temps = args
+    ts = torch.linspace(0, 1000, steps=1000,).view(-1,1).requires_grad_(True).to(DEVICE)
+    temps = model(ts)
+    dT = grad(temps, ts)[0]
+    pde = model.r * (Tenv - temps) - dT
+
+    return torch.mean(pde**2)
 def grad(outputs, inputs):
     """Computes the partial derivative of
     an output with respect to an input.
@@ -118,7 +151,7 @@ def cooling_law(time, Tenv, T0, R):
     T = Tenv + (T0 - Tenv) * np.exp(-R * time)
     return T
 
-def l2_reg(model: torch.nn.Module):
+def l2_reg(model: torch.nn.Module, arg=None):
     return torch.sum(sum([p.pow(2.) for p in model.parameters()]))
 
 
